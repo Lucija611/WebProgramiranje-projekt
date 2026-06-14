2c6f36fa92d5f251e312d1c6cae4625380536492
LAPTOP-5HGEC9FV\lucij <bilandzic.lucija@gmail.com>
2026-06-14 17:50:30 +0200

    Add base template and nav bar

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .forms import RegisterForm
+from notes.models import Profile
 
-# Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            Profile.objects.create(
+                user=user,
+                faculty=form.cleaned_data['faculty']
+            )
+            login(request, user)
+            return redirect('notes:home')
+        else:
+            return render(request, 'accounts/register.html', {'form': form})
+    else:
+        form = RegisterForm()
+        return render(request, 'accounts/register.html', {'form': form})
